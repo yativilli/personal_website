f6f8bf61f7620b92a47bd21febe414cb778f1763
Yannick Wernle <y.wernle.inf21@stud.bbbaden.ch>
2024-07-08 16:22:00 +0200

    Added Class infoService and Project.js Route

---
+++ routes/class/infoService.js
@@ -0,0 +1,25 @@
+var packages = require('../../package.json');
+
+class Info {
+   static getInfo() {
+        return { "name": packages.name, "version": packages.version };
+    }
+
+    static getName(){
+        return packages.name.toString();
+    }
+
+    static getVersion(){
+        return packages.version.toString();
+    }
+
+    static getExpressVersion(){
+        return packages.dependencies.express.toString().replace("^", "");
+    }
+
+    static getJadeVersion(){
+        return packages.dependencies.jade.toString().replace("^", "");
+    }
+}
+
+module.exports = Info
