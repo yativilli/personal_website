08968ca902942945e84fc6278e0799c5b1eea2ef
Yannick Wernle <y.wernle.inf21@stud.bbbaden.ch>
2024-07-15 08:55:22 +0200

    Added Mariadb, and started dbService

--- personal_backend/class/dbService.js
+++ personal_backend/class/dbService.js
@@ -1,5 +1,22 @@
+const mariadb = require('mariadb');
+
 class dbService {
 
+  static async queryDB(res){
+    const pool = mariadb.createPool({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      connectionLimit: 5,
+  })
+  try {
+    const result = await pool.query('SELECT * FROM personal_website.projects');
+    console.log(result);
+    res.send(result);
+   } finally {
+    pool.end();
+   }
+  }
 }
 
-module.exports = Info
+module.exports = dbService
