37ec7b0fa243e9c860492b79c40ad1beab65cb99
Yannick Wernle <y.wernle.inf21@stud.bbbaden.ch>
2024-07-16 09:09:12 +0200

    Added Add-Project Button

--- personal/src/app/Components/my-projects/my-projects.component.ts
+++ personal/src/app/Components/my-projects/my-projects.component.ts
@@ -2,24 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { DbService } from '../../Services/db-service.service';
 import { RouterLink } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-my-projects',
   standalone: true,
-  imports: [
-    RouterLink,
-    MatCardModule
-  ],
+  imports: [RouterLink, MatCardModule, MatIconModule, MatButtonModule],
   templateUrl: './my-projects.component.html',
   styleUrl: './my-projects.component.scss',
 })
-export class MyProjectsComponent implements OnInit{
-
-  constructor(protected dbService: DbService){
-  }
+export class MyProjectsComponent implements OnInit {
+  constructor(protected dbService: DbService) {}
 
   data: any;
   ngOnInit(): void {
-    this.data = this.dbService.getAllProjects();
+    if (this.dbService) {
+      this.data = this.dbService.getAllProjects();
+    }
   }
 }
