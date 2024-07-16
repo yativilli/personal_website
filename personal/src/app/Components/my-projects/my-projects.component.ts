18ec0d04fb668433843a6d0a037ac304044bf4e7
Yannick Wernle <y.wernle.inf21@stud.bbbaden.ch>
2024-07-16 08:09:10 +0200

    Added Rough Layout with the data already comming from the database

--- personal/src/app/Components/my-projects/my-projects.component.ts
+++ personal/src/app/Components/my-projects/my-projects.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { DbService } from '../../Services/db-service.service';
 import { RouterLink } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-my-projects',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    MatCardModule
   ],
   templateUrl: './my-projects.component.html',
   styleUrl: './my-projects.component.scss',
@@ -18,6 +20,6 @@ export class MyProjectsComponent implements OnInit{
 
   data: any;
   ngOnInit(): void {
-    this.data = JSON.stringify(this.dbService.getAllProjects());
+    this.data = this.dbService.getAllProjects();
   }
 }
