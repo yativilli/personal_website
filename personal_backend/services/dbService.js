208ccadab02f8915bbb1a40e97667b23f46fe166
Yannick Wernle <yannick@wernle.net>
2024-07-24 20:55:06 +0200

    Added BackendService

--- personal_backend/services/dbService.js
+++ personal_backend/services/dbService.js
@@ -30,16 +30,54 @@ class dbService {
     }
   }
 
-  static async addProject(project, res){
-
+  static async addProject(project, res) {
+    if (typeof project == "Project") {
+      var projectString = project.id + "," + project.name + "," + project.description + "," + project.repository + "," + project.image + "," + project.in_collaboration_with +
+      "," + project.technology + "," + project.purpose + "," + project.status;
+      const pool = mariadb.createPool({ host: dbhost, user: dbuser, password: dbpassword, connectionLimit: dbconnectionLimit })
+      try {
+        const result = await pool.query('INSERT INTO personal_website.projects (id, name, description, repository, image, in_collaboration_with, technology, purpose, status) VALUES' +
+          '(' + projectString + ');'
+        );
+        res.send(result);
+      } catch (err) {
+        console.log(err)
+      }
+      finally {
+        pool.end();
+      }
+    }
   }
 
-  static async updateProject(project, res){
-    
+  static async updateProject(project, res) {
+    if (typeof project == "Project") {
+      var projectString = "id=" + project.id + ", name=" + project.name + ", description=" + project.description + ", repository=" + project.repository 
+      + ", image=" + project.image + ", in_collaboration_with=" + project.in_collaboration_with +
+      ", technology=" + project.technology + ", purpose=" + project.purpose + ", status=" + project.status;
+      const pool = mariadb.createPool({ host: dbhost, user: dbuser, password: dbpassword, connectionLimit: dbconnectionLimit })
+      try {
+        const result = await pool.query('UPDATE personal_website.projects SET ' + projectString + 'WHERE id=' + project.id.toString());
+        res.send(result);
+      } catch (err) {
+        console.log(err)
+      }
+      finally {
+        pool.end();
+      }
+    }
   }
 
-  static async deleteProject(id, res){
-
+  static async deleteProject(id, res) {
+    const pool = mariadb.createPool({ host: dbhost, user: dbuser, password: dbpassword, connectionLimit: dbconnectionLimit })
+    try {
+      const result = await pool.query('DELETE FROM personal_website.projects WHERE id=' + id.toString());
+      res.send(result);
+    } catch (err) {
+      console.log(err)
+    }
+    finally {
+      pool.end();
+    }
   }
 }
 
